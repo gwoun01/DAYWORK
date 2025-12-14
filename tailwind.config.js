799ec9ce6bf230d8b5b4f141f4284aa60611c473
gwoun01 <gwoun01@naver.com>
2025-12-14 04:47:28 +0000

    css

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  content: [
+    "./docs/**/**/*.{html,js,css}",
+    "./src/**/*.{ts,js}"
+  ],
+  safelist: [
+    "bg-green-50",
+    "bg-blue-50",
+    "bg-purple-50"
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
